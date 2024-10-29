bec8073e12ce07c5b76a72cc696844637fb90699
gpopowic <gmpopowich@gmail.com>
2024-10-29 16:06:18 -0700

    added sign-in, navbar, header and updated routes in main and modified app

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar/Navbar";
+import Header from "./Header/Header";
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignInPage from "./auth/sign-in/sign-in";
+import { Home } from "./Home/Home";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/auth/sign-in",
+    element: <SignInPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
